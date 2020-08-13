cb70a0b381e2881a6df25739edfd70ec5359f5c9
antiproton85 <kevinmcgr@gmail.com>
2020-08-13 10:52:20 -0400

    Update config to use K8s secrets instead of flat files

--- config.py
+++ config.py
@@ -1,15 +1,28 @@
 import jsonpickle
+import os
 
 from pathlib import Path
 
 from models.config import LogType
 
-config_path = Path("./config.json")
+deploy_type = os.environ.get('DEPLOY_TYPE')
+
+if deploy_type == 'prod':
+    config_path = '/etc/public_housekeeper/bot_config/config.json'
+else:
+    config_path = Path("./config.json")
 
 with open(config_path, 'r') as _config_file:
     _config = jsonpickle.decode(_config_file.read())
 
 bot_configs = {bot['name']: bot for bot in _config['bots']}
+
+if deploy_type == 'prod':
+    bot_configs['public']['secrets_folder'] = '/etc/public_housekeeper/bot_config'
+    bot_configs['corp']['secrets_folder'] = '/etc/public_housekeeper/bot_config'
+
+
+
 salesforce = _config['salesforce']
 
 LogConfig = LogType(_config['logging'])
